e43e45a75257026c20759b127420951da632125b
SorinBogdan <tzully.007@gmail.com>
2017-04-19 21:35:58 -0200

    updated buttons.py

--- buttons.py
+++ buttons.py
@@ -1,13 +1,33 @@
 from time import sleep
 import os
 import RPi.GPIO as GPIO
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(4,GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
+BTN_LEFT    = 26
+BTN_RIGHT   = 13
+BTN_ROTATE  = 19
+BTN_NOBUTTON= 0
 
-while True:
-    if GPIO.input(4) == False:
-        print("apasat")
+def initButtons():
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(BTN_LEFT,GPIO.IN, pull_up_down=GPIO.PUD_UP)
+    GPIO.setup(BTN_RIGHT,GPIO.IN, pull_up_down=GPIO.PUD_UP)
+    GPIO.setup(BTN_ROTATE,GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+def getPressedButton():
+    if GPIO.input(BTN_LEFT)==False:
+        return BTN_LEFT
+    elif GPIO.input(BTN_RIGHT)==False:
+        return BTN_RIGHT
+    elif GPIO.input(BTN_ROTATE)==False:
+        return BTN_ROTATE
     else:
-        print("neapasat")
-    sleep(0.1)
+        return BTN_NOBUTTON
+
+def main():
+    initButtons()
+    while True:
+        print(getPressedButton())
+        sleep(0.1)
+        
+if __name__ == '__main__':
+    main()
